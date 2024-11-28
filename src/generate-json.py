4366b9e50b6611a1e27de7a171c8684c3ec885d8
Bruno César de Sena <brunocdesenadesenvolvedor@gmail.com>
2024-11-28 00:21:18 -0300

    first commit

---
+++ src/generate-json.py
@@ -0,0 +1,33 @@
+from os import listdir, path
+from json import dump
+from time import sleep
+
+def generate_json():
+    base_path = 'C:\\Users\\User\\Documents\\b2-frotas-page\\public\\EmpresasImagensWebp'
+    output_file = 'C:\\Users\\User\\Documents\\b2-frotas-page\\public\\empresaswebp.json'
+    list = []
+    
+    for folder in listdir(base_path):
+        folder_path = path.join(base_path, folder)
+
+        if path.isdir(folder_path):
+            images = [
+                f'/EmpresasImagensWebp/{folder}/{file}'
+                for file in listdir(folder_path)
+                if file.endswith(('.webp'))
+            ]
+            list.append(
+                {
+                    "nome": folder,
+                    "imagens": images
+                }
+            )
+            
+    with open(output_file, 'w', encoding='utf-8') as json_file:
+        dump(list, json_file, ensure_ascii=False, indent=4)
+    
+    print(f"JSON gerado com sucesso em {output_file}!")
+
+    sleep(3)
+
+generate_json()
